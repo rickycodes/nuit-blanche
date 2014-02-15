7778c1cacc0a07cd97d0d3c882fe22169ef9a145
omega <ricky.miller@gmail.com>
2014-02-15 07:57:45 -0500

    changed some things, sphere to planes

--- app.js
+++ app.js
@@ -1,95 +1,97 @@
-var express = require('express')
-  , app = express()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server)
-  , twitter = require('ntwitter')
-  , path = require('path')
-  , request = require('request')
-  , fs = require('fs')
-  , sys = require('sys')
-  , exec = require('child_process').exec
-  , https = require('https')
-  , conf = require('./config')
+var express = require('express'),
+  app = express(),
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(server),
+  twitter = require('ntwitter'),
+  path = require('path'),
+  request = require('request'),
+  fs = require('fs'),
+  sys = require('sys'),
+  exec = require('child_process').exec,
+  https = require('https'),
+  conf = require('./config');
 
 if(conf.twitter.consumer_key === '') {
   console.log('please fill out config.js first!');
   process.exit(1);
 }
 
-var i = 1
-  , track = conf.track
-  , img_path = conf.img_path
-  , del = conf.del
-  , t = new twitter(conf.twitter)
+var i = 1,
+  track = conf.track,
+  img_path = conf.img_path,
+  del = conf.del,
+  t = new twitter(conf.twitter);
 
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jade')
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, 'public/img')))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/img')));
 
 app.get('/', function(req, res) {
-  res.render('index')
-})
+  res.render('index');
+});
 
 function requestURL( url, data, img_name ) {
 
   request(url, function(error, response, body) {
     if(error) {
-      console.log(error)
-      console.log(url)
+      console.log(error);
+      console.log(url);
     }
     if(!error && response.statusCode === 200) {
-      io.sockets.emit('add planet', {tweet: data, img: img_name})
+      io.sockets.emit('add planet', {tweet: data, img: img_name});
     }
-  }).pipe(fs.createWriteStream(img_path + img_name))
+  }).pipe(fs.createWriteStream(img_path + img_name));
 }
+
+console.log(t);
 
 t.stream('statuses/filter', {'track':track}, function(stream) {
   stream.on('data', function(data) {
 
-    var jpg_name = i + '.jpg'
-    var url
+    var jpg_name = i + '.jpg';
+    var url;
 
     // promises, plz!
 
     if(data.entities.media && data.entities.media.length) {
-      url = data.entities.media[0].media_url
-      requestURL(url, data, jpg_name)
+      url = data.entities.media[0].media_url;
+      requestURL(url, data, jpg_name);
     } else if(data.entities.urls && data.entities.urls.length) {
         if(data.entities.urls[0].expanded_url) {
           if(data.entities.urls[0].expanded_url.match(/instagram.com\/p/)) {
-            url = 'http://' + data.entities.urls[0].display_url + 'media/?size=m'
-            requestURL(url, data, jpg_name)
+            url = 'http://' + data.entities.urls[0].display_url + 'media/?size=m';
+            requestURL(url, data, jpg_name);
           }
         } else {
           // no twitter, no instagram
           // send plain tweet and/or video tweet
-          var url = data.user.profile_image_url.replace('_normal','');
-          requestURL(url, data, jpg_name)
+          url = data.user.profile_image_url.replace('_normal','');
+          requestURL(url, data, jpg_name);
         }
     } else {
 
       // no twitter, no instagram
       // send plain tweet
-      var url = data.user.profile_image_url.replace('_normal','');
-      requestURL(url, data, jpg_name)
+      url = data.user.profile_image_url.replace('_normal','');
+      requestURL(url, data, jpg_name);
     }
-  })
-})
+  });
+});
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('init','application (re)started')
+  socket.emit('init','application (re)started');
   socket.on('delete', function(data) {
-    i++
+    i++;
     if(del) {
       fs.unlink(img_path + data.url, function() {
         console.log('image deleted');
-      })
+      });
     }
-  })
-})
+  });
+});
 
-server.listen(conf.port)
+server.listen(conf.port);
 
-console.log('app started on http://' + conf.host + ':' + conf.port)
+console.log('app started on http://' + conf.host + ':' + conf.port);
--- public/init.js
+++ public/init.js
@@ -11,54 +11,32 @@ var NuitBlanche = (function() {
   halfy = window.innerHeight / 2,
 
   container,
-  stars = [],
+
   camera,
   scene,
   renderer,
   group,
-  program,
+
   area,
   planets,
+  
   tick,
   goal,
+  
   boom,
 
   mousex = 0,
   mousey = 0,
 
-  warpZ = 12,
-  units = 100,
-  cycle = 0,
-  Z = 0.1,
-
   intro = document.getElementsByClassName('intro'),
   
-  socket = io.connect('http://' + window.location.host + '/'),
-  starsCanvas = d.createElement('canvas');
-
-  starsCanvas.setAttribute('class','stars');
-  starsCanvas.setAttribute('width', width);
-  starsCanvas.setAttribute('height', height);
-
-  d.body.appendChild(starsCanvas);
-
-  var G=starsCanvas.getContext("2d");
-  G.globalAlpha=0.25;
+  socket = io.connect('http://' + window.location.host + '/');
 
   function randNum(min,max) {
     return rnd() * (max - min + 1) + min;
   }
 
   function init() {
-
-    (function createStars() {
-      for (var i = 0, n; i < units; i++)
-      {
-        n = {};
-        resetstar(n);
-        stars.push(n);
-      }
-    })();
 
     container = d.createElement('div');
     container.setAttribute('class','container');
@@ -67,8 +45,8 @@ var NuitBlanche = (function() {
 
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 4000);
     camera.position.z = 1000;
+    
     scene = new THREE.Scene();
-    var PI2 = Math.PI * 2;
     
     area = 200;
     boom = false;
@@ -89,61 +67,6 @@ var NuitBlanche = (function() {
     console.log('universe (re)initiated');
 
     bindEvents();
-  }
-
-  function resetstar(a)
-  {
-     a.x = (rnd() * width - (width * 0.5)) * warpZ;
-     a.y = (rnd() * height - (height * 0.5)) * warpZ;
-     a.z = warpZ;
-     a.px = 0;
-     a.py = 0;
-  }
-
-  // star rendering anim function
-  function rf()
-  {
-    // clear background
-    G.fillStyle = "#000";
-    G.fillRect(0, 0, width, height);
-
-    // mouse position to head towards
-    var cx = (width / 2),
-        cy = (height / 2);
-     
-    // update all stars
-    var sat = floor(Z * 500);       // Z range 0.01 -> 0.5
-    if (sat > 100) sat = 100;
-    for (var i=0; i<units; i++) {
-      var n = stars[i],            // the star
-        xx = n.x / n.z,          // star position
-        yy = n.y / n.z,
-        e = (1.0 / n.z + 1) * 2;   // size i.e. z
-        
-      if (n.px !== 0) {
-        // hsl colour from a sine wave
-        G.strokeStyle = '#FFF';
-        G.lineWidth = e;
-        G.beginPath();
-        G.moveTo(xx + cx, yy + cy);
-        G.lineTo(n.px + cx, n.py + cy);
-        G.stroke();
-      }
-        
-      // update star position values with new settings
-      n.px = xx;
-      n.py = yy;
-      n.z -= Z;
-        
-      // reset when star is out of the view field
-      if (n.z < Z || n.px > width || n.py > height) {
-        // reset star
-        resetstar(n);
-      }
-    }
-     
-    // colour cycle sinewave rotation
-    cycle += 0.01;
   }
 
   function bindEvents() {
@@ -177,9 +100,6 @@ var NuitBlanche = (function() {
     camera.updateProjectionMatrix();
 
     renderer.setSize(window.innerWidth, window.innerHeight);
-
-    starsCanvas.setAttribute('width', window.innerWidth);
-    starsCanvas.setAttribute('height', window.innerWidth);
   }
 
   function onTouchStart(event) {
@@ -202,7 +122,6 @@ var NuitBlanche = (function() {
     requestAnimationFrame(animate);
     
     render();
-    rf();
   }
 
   function render() {
@@ -243,17 +162,16 @@ var NuitBlanche = (function() {
 
   var Planet = function(group, texture) {
 
-    var sphere_material = new THREE.MeshBasicMaterial({map: texture}), segments = 40;
+    var material = new THREE.MeshBasicMaterial( {map: texture} ), segments = 40;
+    material.side = THREE.DoubleSide;
 
-    this.w = randNum(100,180);
-
-    this.planet = new THREE.Mesh(new THREE.SphereGeometry(this.w, segments, segments),sphere_material);
+    this.planet = new THREE.Mesh( new THREE.PlaneGeometry( 400, 400, segments), material );
 
     this.planet.position.x = rnd() * area * 2 - area;
     this.planet.position.y = rnd() * area * 2 - area;
     this.planet.position.z = rnd() * area * 2 - area;
 
-    group.add(this.planet);
+    group.add( this.planet );
   };
    
   Planet.prototype.expand = function() {
